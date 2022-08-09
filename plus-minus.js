81738cf4d272d947e7bbda82a3d39178ffc8d7b3
Shirinapr <shirinapr1@gmail.com>
2022-08-09 14:13:30 +0430

    add: plus minus.

---
+++ plus-minus.js
@@ -0,0 +1,21 @@
+function main(arr) {
+    let positive = 0;
+    let negative = 0;
+    let zero = 0;
+    let length = arr.length;
+  
+    for (let i = 0; i < length; ++i) {
+      if (arr[i] > 0) {
+        positive++;
+      } else if (arr[i] < 0) {
+        negative++;
+      } else {
+        zero++;
+      }
+    }
+    // this algorithm dosen't have return.
+
+    console.log((positive / length).toFixed(6));
+    console.log((negative / length).toFixed(6));
+    console.log((zero / length).toFixed(6));
+  }
