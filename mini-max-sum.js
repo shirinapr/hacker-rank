2089386e9d543e8f1324e2160a435642079de026
shirinapr <shirinapr1@gmail.com>
2022-08-13 01:43:26 +0430

    add: mini-max-sum.

---
+++ mini-max-sum.js
@@ -0,0 +1,9 @@
+function main(arr) {
+  let sum = arr.reduce((a, b) => a + b);
+  let minNumber = Math.min(...arr);
+  let maxNumber = Math.max(...arr);
+
+  // sum-maxNumber is Minimum
+  // sum-minNumber is Maximum
+  console.log(sum - maxNumber, sum - minNumber);
+}
