fd835dfeb2a8b562abec75da514a1349773c9101
Shirinapr <shirinapr1@gmail.com>
2022-08-14 18:48:28 +0430

    add: birthday-cake-candles.

---
+++ birthday-cake-candles.js
@@ -0,0 +1,12 @@
+function main(candles) {
+  let max = Math.max(...candles);
+  let count = 0;
+
+  for (let i = 0; i < candles.length; ++i) {
+    if (candles[i] === max) {
+      count++;
+    }
+  }
+
+  return count;
+}
